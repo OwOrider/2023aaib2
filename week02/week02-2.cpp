936822f533c6aecd9c42f6bb1ea0e2670b92bfdc
OwOrider <paz.4613@gmail.com>
2024-03-01 11:36:07 +0800

    week02

---
+++ week02/week02-2.cpp
@@ -0,0 +1,14 @@
+///璶粄醚float疊翴计(floating ponit number)
+///doubleㄢ疊翴计(double floating ponit number)
+#include <stdio.h>
+int main()
+{///糶ê或秆狦C粂ē(CPU笲い)柑ノぶぶ计
+    float pi=3.14159265358979328462643383279;
+    double pi2=3.14159265358979328462643383279;
+    printf("float %f\n",pi);
+    printf("double %f\n",pi2);
+    printf("float 10: %10f\n",pi);
+    printf("double 10: %10f\n",pi2);
+    printf("float 10: %20.18f\n",pi);
+    printf("double 10: %20.18f\n",pi2);
+}

74762cbaa522ec8fac4f16157f22489ed236ecac
OwOrider <paz.4613@gmail.com>
2024-03-15 11:35:21 +0800

    week04

---
+++ week04/week04-5.cpp
@@ -0,0 +1,18 @@
+///Allman(ANSI)
+#include <stdio.h>
+int main()
+{
+    int n,a,b;
+    scanf("%d",&n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a);
+        int ans=0;
+        for(int j=0; j<a; j++)
+        {
+            scanf("%d",&a);
+            ans +=b;
+        }
+        printf("%d\n",ans);
+    }
+}

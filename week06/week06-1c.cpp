f50e96274ceeb6706324111060afa14d951b8194
OwO <>
2024-03-29 11:46:39 +0800

    week06

---
+++ week06/week06-1c.cpp
@@ -0,0 +1,17 @@
+#include stdio.h
+int main()
+{
+	int n;
+	
+	scanf(%d,&n);
+	for(i=1;i=n;i++)
+	{
+		int star=i,space=n-space;
+		
+		for(int k=0;kspace;k++)
+			printf( );
+		for(int k=0;kspace;j++)
+			printf();
+		printf(n);
+	}
+}

deff7fcaa97de8e6bb86932b9ce0ee84ee809aaf
OwOrider <145088352+OwOrider@users.noreply.github.com>
2024-05-31 11:30:55 +0800

    Add files via upload

---
+++ week15/week15-4.cpp
@@ -0,0 +1,11 @@
+///week15-4
+#include <stdio.h>
+int main()
+{
+    if('A'==65) printf("字母A竟然和65相等\n");
+    printf("字母A的值是:%d\n",65,'A');
+    printf("字母B的值是:%d\n",66,'B');
+    printf("字母C的值是:%d\n",67,'C');
+    printf("\n");
+    printf("字母%c的值是:%d\n",97,'a');
+}
